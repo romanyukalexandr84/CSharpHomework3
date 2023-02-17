1d8115c540a87f7ff6fde089ad4f6fc8f8801753
romanukalexandr <romanukalexandr@gmail.com>
2023-02-18 01:57:21 +0300

    add Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine ("Введите целое число N");
+int number = Convert.ToInt32 (Console.ReadLine());
+int index = 1;
+Console.WriteLine ("Кубы чисел от 1 до N включительно:");
+if (number>=1)
+{
+    while (index <= number)
+{
+    Console.Write (Math.Pow(index,3) + " ");
+    index++;
+}
+}
+else
+{
+    while (index >= number)
+{
+    Console.Write (Math.Pow(index,3) + " ");
+    index--;
+}
+}
+Console.WriteLine();
