16665bea529cb64c6e11b7ddb1ca5cea01124812
romanukalexandr <romanukalexandr@gmail.com>
2023-02-17 23:37:42 +0300

    add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿int Check (int number)
+{
+while (number < 10000 || number > 99999)
+{
+    Console.WriteLine ("Число не является пятизначным, повторите ввод");
+    number = Convert.ToInt32 (Console.ReadLine());
+}
+return number;
+}
+
+string Pal (int digit)
+{
+    if ((digit / 10000 == digit % 10) && (digit / 1000 % 10 == digit / 10 % 10))
+    {
+        return "Это палиндром";
+    }
+    else
+    {
+        return "Это не палиндром";
+    }
+}
+
+Console.WriteLine ("Введите пятизначное число");
+int count = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine (Pal (Check (count)));
